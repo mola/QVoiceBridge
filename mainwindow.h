f6c011fd391a8297453479acce262bdf237db114
Mola Pahnadayan <mola.mp@gmail.com>
2025-02-23 13:15:34 +0330

    First Initilize

---
+++ mainwindow.h
@@ -0,0 +1,40 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTextToSpeech>
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+public slots:
+    void setRate(int rate);
+    void setPitch(int pitch);
+    void setVolume(int volume);
+
+private slots:
+
+    void engineSelected(int index);
+    void languageSelected(int language);
+    void voiceSelected(int index);
+    void stateChanged(QTextToSpeech::State state);
+    void localeChanged(const QLocale &locale);
+    void onEngineReady();
+private:
+    Ui::MainWindow *ui;
+    QTextToSpeech *m_speech = nullptr;
+    QList<QVoice> m_voices;
+};
+#endif // MAINWINDOW_H
